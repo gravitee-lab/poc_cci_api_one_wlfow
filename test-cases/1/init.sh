28be07e27f8aaaf45761666e42335c355602b2fc
Jean-Baptiste-Lasselle <jean.baptiste.lasselle.pegasus@gmail.com>
2020-09-18 00:01:35 +0200

    correction of a syntax typo in test case one init script

--- test-cases/1/init.sh
+++ test-cases/1/init.sh
@@ -7,12 +7,12 @@ git checkout ${WORK_BRANCH}
 
 
 
-git checkout -D support-one-testcase1
-git checkout -D issue-one-testcase1
-git checkout -D issue-two-testcase1
-git checkout -D support-two-testcase1
-git checkout -D issue-three-testcase1
-git checkout -D issue-four-testcase1
+git branch -D support-one-testcase1
+git branch -D issue-one-testcase1
+git branch -D issue-two-testcase1
+git branch -D support-two-testcase1
+git branch -D issue-three-testcase1
+git branch -D issue-four-testcase1
 
 
 git checkout 3.0.x
